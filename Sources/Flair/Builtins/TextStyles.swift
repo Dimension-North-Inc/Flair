c93bf3cd9235ba3604e2e5ffac376aadad2d59ac
Mark Onyschuk <mark.onyschuk@ndimensionl.com>
2023-09-29 18:10:11 -0500

    Add `Equatable` conformance to `Style`
    Adding an additional constraint to StyleKeys:  `Codable & Equatable` has allowed us to add `Equatable` conformance to `Style`.
    
    The custom implementation of Equatable resembles our earlier implementation of Codable, where the `StyleKeys` type is leveraged to compare individual style key values for equivalence.

--- Sources/Flair/Builtins/TextStyles.swift
+++ Sources/Flair/Builtins/TextStyles.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2023 Dimension North Inc. All rights reserved.
 //
 
+import SwiftUI
 import Foundation
 
 extension Style.Key {
@@ -15,34 +16,90 @@ extension Style.Key {
         case centered
         case justified
 
-        public static var name      = "flair.text.alignment"
+        public static var name      = "flair.alignment"
         public static var initial   = Self.leading
+    }
+
+    public struct LineSpacingStyle: StyleKeys {
+        public static var name      = "flair.line-spacing"
+        public static var initial   = CGFloat(0)
+    }
+    
+    public struct LineHeightMultipleStyle: StyleKeys {
+        public static var name      = "flair.line-height-multiple"
+        public static var initial   = CGFloat(0)
+    }
+    
+    public struct ParagraphSpacingStyle: StyleKeys {
+        public static var name      = "flair.paragraph-spacing"
+        public static var initial   = CGFloat(0)
+    }
+
+    public struct ParagraphSpacingBeforeStyle: StyleKeys {
+        public static var name      = "flair.paragraph-spacing-before"
+        public static var initial   = CGFloat(0)
     }
     
     public struct ForegroundColorStyle: StyleKeys {
-        public static var name      = "flair.text.color"
+        public static var name      = "flair.foreground-color"
         public static var initial   = Color.primary
     }
     
     public struct BackgroundColorStyle: StyleKeys {
-        public static var name      = "flair.text.background-color"
+        public static var name      = "flair.background-color"
         public static var initial   = Color.clear
     }
     
-    public var text: Text {
-        return Text()
+    public var text: TextKeys {
+        return TextKeys()
     }
-    public struct Text {
+    public struct TextKeys {
         fileprivate init() {}
         
         public var alignment: AlignmentStyle.Type {
             AlignmentStyle.self
         }
+        
         public var color: ForegroundColorStyle.Type {
             ForegroundColorStyle.self
         }
         public var backgroundColor: BackgroundColorStyle.Type {
             BackgroundColorStyle.self
         }
+
+        public var lineSpacing: LineSpacingStyle.Type {
+            LineSpacingStyle.self
+        }
+        public var lineHeightMultiple: LineHeightMultipleStyle.Type {
+            LineHeightMultipleStyle.self
+        }
+
+        public var paragraphSpacing: ParagraphSpacingStyle.Type {
+            ParagraphSpacingStyle.self
+        }
+        public var paragraphiSpacingBefore: ParagraphSpacingBeforeStyle.Type {
+            ParagraphSpacingBeforeStyle.self
+        }
+    }
+}
+
+extension Style {
+    var paragraphStyle: NSParagraphStyle {
+        let result = NSMutableParagraphStyle()
+        
+        switch self[\.text.alignment] {
+        case .leading:   result.alignment = .left
+        case .trailing:  result.alignment = .right
+        case .centered:  result.alignment = .center
+        case .justified: result.alignment = .justified
+        }
+        
+        result.lineSpacing = self[\.text.lineSpacing]
+        result.paragraphSpacing = self[\.text.paragraphSpacing]
+        result.paragraphSpacingBefore = self[\.text.paragraphiSpacingBefore]
+        
+        result.lineHeightMultiple = self[\.text.lineHeightMultiple]
+
+        return result
     }
 }
